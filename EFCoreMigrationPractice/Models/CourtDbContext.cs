04cc73ea9b58f5493c61d3a1ac0d1af2a003f665
Wu Xueheng <xhwu@thoughtworks.com>
2020-11-09 16:18:48 +0800

    feat: add model and db context

---
+++ EFCoreMigrationPractice/Models/CourtDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreMigrationPractice.Models
+{
+    public class CourtDbContext : DbContext
+    {
+        public CourtDbContext(DbContextOptions<CourtDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Prosecutor> Prosecutor { get; set; }
+    }
+}
